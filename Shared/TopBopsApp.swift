71dd71113c778d89321efcd8e63d1c70ea63b3a5
CJ Gaspari <cjgaspari@users.noreply.github.com>
2020-07-24 12:19:22 -0700

    Initial Commit

---
+++ Shared/TopBopsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TopBopsApp.swift
+//  Shared
+//
+//  Created by CJ Gaspari on 7/24/20.
+//
+
+import SwiftUI
+
+@main
+struct TopBopsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
